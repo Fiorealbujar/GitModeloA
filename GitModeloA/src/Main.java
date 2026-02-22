0c2ac32c608c27fdc462bdff98211adf105a922e
Fiorella Albujar <115477583+Fiorealbujar@users.noreply.github.com>
2026-02-22 22:46:57 +0100

    Cambio desde remoto

--- GitModeloA/src/Main.java
+++ GitModeloA/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hola mundo");
 		System.out.println("Hola a todo@s");
 		System.out.println("Trabajo en RamaA");
+		System.out.println("Trabajo en remoto");
 	}
 
 }
